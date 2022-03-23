e46a66aedbf47ad6f724205ea140cf2ecc398955
Maxim Azarkov <aza-maxim@yandex.ru>
2022-03-24 01:43:27 +0700

    реализована задача формаирования списка дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,14 +6,17 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        HashMap<FileProperty, List<Path>> listDoubles = new HashMap<>();
+        Map<FileProperty, List<Path>> listDoubles = new HashMap<>();
         List<Path> ld = new LinkedList<>();
         DuplicatesVisitor dv = new DuplicatesVisitor();
         ld.add(Files.walkFileTree(Path.of("./"), dv));
         listDoubles = dv.getDoubles();
+        System.out.println("---");
+        System.out.println(listDoubles);
 
         System.out.println(listDoubles.size());
     }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,29 +5,44 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    HashMap<FileProperty, List<Path>> listDoubles = new HashMap<>();
+    HashMap<FileProperty, List<Path>> fullList = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (!file.toFile().isDirectory()) {
 
             var fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
-            var listPath = listDoubles.get(fileProperty);
+            var listPath = fullList.get(fileProperty);
             if (listPath == null) {
                 listPath = new LinkedList<>();
-                listPath.add(file);
-                listDoubles.put(new FileProperty(file.toFile().length(), file.getFileName().toString()), listPath);
             }
+            listPath.add(file);
+            fullList.put(new FileProperty(file.toFile().length(), file.getFileName().toString()), listPath);
+
+            if (listPath.size() > 1) {
+                System.out.println(listPath);
+            }
+
         }
         return FileVisitResult.CONTINUE;
     }
 
-    public HashMap<FileProperty, List<Path>> getDoubles() {
+    /**
+     * возвращает список дубликатов для дальнейшей обработки
+     * @return - список дубликатов
+     */
+    public Map<FileProperty, List<Path>> getDoubles() {
+        Map<FileProperty, List<Path>> listDoubles = new HashMap<>();
+        Iterator<Map.Entry<FileProperty, List<Path>>> entries = fullList.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<FileProperty, List<Path>> entry = entries.next();
+            if (entry.getValue().size() > 1) {
+                listDoubles.put(entry.getKey(), entry.getValue());
+            }
+        }
         return listDoubles;
     }
 }
