53a577325fb6070aa6ac914480a20f0c598cfce7
Maxim Azarkov <aza-maxim@yandex.ru>
2022-03-26 08:02:10 +0700

    реализована задача формаирования списка дубликатов.refactoring#2

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,21 +3,9 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Map<FileProperty, List<Path>> listDoubles = new HashMap<>();
-        List<Path> ld = new LinkedList<>();
-        DuplicatesVisitor dv = new DuplicatesVisitor();
-        ld.add(Files.walkFileTree(Path.of("./"), dv));
-        listDoubles = dv.getDoubles();
-        System.out.println("---");
-        System.out.println(listDoubles);
-
-        System.out.println(listDoubles.size());
+        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
     }
 }
