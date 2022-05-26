00520f8d63a3db3df60d0e8bf7ee1601d896fd48
Maxim Azarkov <aza-maxim@yandex.ru>
2022-05-26 16:14:58 +0700

    5. Вызов переопределенного метода родителя - super.method ...   [#504871]

---
+++ src/main/java/ru/job4j/inheritance/IceCream.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class IceCream {
+    private final int weight;
+    public IceCream(int weight) {
+        this.weight = weight;
+    }
+
+    public int price() {
+        return weight * 100;
+    }
+}
